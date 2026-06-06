87e4b6f83cf02716dd2142055743d23fc0d5ab0e
Snapky06 <saidnapky@gmail.com>
2026-06-06 14:12:07 -0600

    Index finder and ordered insertion

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -1,5 +1,5 @@
 #include "StudentManager.hpp"
-
+#include <cstring>
 bool StudentManager::addRegister(std::string file)
 {
     
@@ -39,5 +39,48 @@ bool StudentManager::saveIndex()
         f.write(reinterpret_cast<char*>(&indexes[i]),sizeof(index));
         if(!f)return false;
     }
+
 return !f.fail();
+}
+
+int StudentManager::findIndexPosition(std::string account)
+{
+    loadIndex();
+    int left = 0;
+    int right = indexes.size() - 1;
+
+    while(left<=right){
+        int mid = left + (right - left)/2;
+        int cmp = strcmp(indexes[mid].account,account.c_str());
+
+        if(cmp == 0){
+            return mid;
+        }else if(cmp < 0){
+            left = mid + 1;
+        }else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+bool StudentManager::insertIndexOrdered(index new_index)
+{
+    loadIndex();
+    for(int i = 0;i< indexes.size() - 1; i++){
+
+        int cmp = strcmp(indexes[i].account,new_index.account);
+        
+        if(cmp == 0)return false;
+
+        if(cmp > 0){
+            indexes.insert(indexes.begin() + i, new_index);
+            saveIndex();
+            return true;
+        }
+    }
+
+    indexes.push_back(new_index);
+    saveIndex();
+    return  false;;
 }
